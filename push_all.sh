7534f2ea86f52761ceec8d7dc6e1313bf6e83ba4
David Wu <dwu@humana.com>
2020-09-29 13:13:14 -0500

    Added push tag

--- push_all.sh
+++ push_all.sh
@@ -4,6 +4,6 @@ do
    echo "Process $i..."
    echo "    - Creating Random File.."
    base64 /dev/urandom | head -c 26214400 > $i/data.file
-   echo "    - Starting CF Push"
+   echo "    - Starting CF Push $1"
    (time ./$i/push.sh sample_${i}_${1}) &> ${i}_push.log
 done 
