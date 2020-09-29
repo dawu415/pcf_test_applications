a85eef44b5d4db9687183af2bba5522e69dcef06
David Wu <dwu@humana.com>
2020-09-29 12:54:51 -0500

    updated push script for verbose output

--- push_all.sh
+++ push_all.sh
@@ -1,6 +1,9 @@
-#! /usr/bin/bash
+#!/usr/bin/bash
 for i in "aspnet-mvc-sample-app" "golang-sample-app" "ruby-sample-app" "spring-music";
 do
+   echo "Process $i..."
+   echo "Creating Random File.."
    base64 /dev/urandom | head -c 26214400 > $i/data.file
+   echo "Starting CF Push"
    (time ./$i/push.sh sample_$i) &> ${i}_push.log
 done 
